976d6d61fb84338f082026a0224f255ddc73c0ef
sanco <sansunder718jqda@gmail.com>
2025-02-10 20:12:34 +0100

    Added extensions to the renderer to take screenshots.

--- LivinOnSweets/LivinOnSweets.API/Extensions/RendererExtensions.cs
+++ LivinOnSweets/LivinOnSweets.API/Extensions/RendererExtensions.cs
@@ -1,5 +1,9 @@
-﻿using osu.Framework.Graphics.Rendering;
+﻿using System.Reflection;
+using osu.Framework.Graphics.Rendering;
+using osu.Framework.Graphics.Textures;
 using osu.Framework.Threading;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace LivinOnSweets.API.Extensions
 {
@@ -11,6 +15,28 @@ namespace LivinOnSweets.API.Extensions
             ScheduledDelegate operation = new(task, executionTime, repeatInterval);
             renderer.ScheduleExpensiveOperation(operation);
             return operation;
+        }
+
+        public static Image<Rgba32> TakeScreenshotToImage(this IRenderer renderer)
+        {
+            // Since the TakeScreenshot function is internal we'll have to use reflection to get the function
+            Type renderType = renderer.GetType();
+
+            MethodInfo method = renderType.GetMethod("TakeScreenshot", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (method == null)
+                return null;
+
+            return (Image<Rgba32>)method.Invoke(renderer, null);
+        }
+
+        public static Texture TakeScreenshotToTexture(this IRenderer renderer)
+        {
+            Image<Rgba32> image = renderer.TakeScreenshotToImage();
+
+            Texture texture = renderer.CreateTexture(image.Width, image.Height);
+            texture.SetData(new TextureUpload(image));
+
+            return texture;
         }
     }
 }
