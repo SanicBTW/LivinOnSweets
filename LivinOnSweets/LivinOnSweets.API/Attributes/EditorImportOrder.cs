ae896633aaf341eb82489db9c264180aea05e55e
sanco <sansunder718jqda@gmail.com>
2025-02-10 20:08:03 +0100

    Move the editor to its own package, remove the old editor code.
    The layout is going to be updated.

--- LivinOnSweets/LivinOnSweets.API/Attributes/EditorImportOrder.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LivinOnSweets.API.Attributes
-{
-    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-    public class EditorImportOrder(int position) : Attribute
-    {
-        public int ImportPosition = position;
-    }
-}
--- LivinOnSweets/LivinOnSweets.API/Enum/EditorEntryContentAnimation.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LivinOnSweets.API.Enum
-{
-    // Represents the type of content animation the EditorEntry will have
-    public enum EditorEntryContentAnimation
-    {
-        EXPANDABLE,
-        ANOTHER_VIEW
-    }
-}
