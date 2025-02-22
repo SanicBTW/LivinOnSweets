49a410baeb0fe010e21c41e00420b42c5d6c3f7b
sanco <sansunder718jqda@gmail.com>
2025-02-22 22:56:46 +0100

    Added Backdrop with a custom shader for it.
    I actually really like this one

---
+++ LivinOnSweets/LivinOnSweets.API/Sprites/Backdrop.cs
@@ -0,0 +1,108 @@
+﻿using System.Runtime.InteropServices;
+using JetBrains.Annotations;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Rendering;
+using osu.Framework.Graphics.Shaders;
+using osu.Framework.Graphics.Shaders.Types;
+using osu.Framework.Graphics.Sprites;
+
+namespace LivinOnSweets.API.Sprites
+{
+    public partial class Backdrop : Sprite
+    {
+        protected override DrawNode CreateDrawNode() => new BackdropDrawNode(this);
+
+        public readonly double Duration;
+
+        public bool Running { get; private set; }
+
+        public Backdrop(double duration = 2000D)
+        {
+            RelativeSizeAxes = Axes.Both;
+            Duration = duration;
+        }
+
+        public void Start()
+        {
+            if (Running)
+                return;
+
+            Running = true;
+            Invalidate(Invalidation.DrawNode);
+        }
+
+        public void Freeze()
+        {
+            if (!Running)
+                return;
+
+            Running = false;
+            Invalidate(Invalidation.DrawNode);
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(ShaderManager shaders)
+        {
+            TextureShader = shaders.Load(@"Backdrop", @"Backdrop");
+        }
+
+        private partial class BackdropDrawNode : SpriteDrawNode
+        {
+            public new Backdrop Source => (Backdrop)base.Source;
+
+            [CanBeNull] private IUniformBuffer<BackdropTileParameters> parametersBuffer;
+            private float tileScaleX = 1;
+            private float tileScaleY = 1;
+            private float scrollSpeed;
+
+            public BackdropDrawNode(Backdrop source) : base(source) { }
+
+            public override void ApplyState()
+            {
+                base.ApplyState();
+
+                tileScaleX = Source.DrawWidth / Texture.DisplayWidth;
+                tileScaleY = Source.DrawHeight / Texture.DisplayHeight;
+
+                if (!Source.Running || Source.Duration <= 0)
+                    scrollSpeed = 0;
+                else
+                    scrollSpeed = (0.001f / (float)(Source.Duration / 1000.0));
+            }
+
+            protected override void BindUniformResources(IShader shader, IRenderer renderer)
+            {
+                base.BindUniformResources(shader, renderer);
+
+                parametersBuffer ??= renderer.CreateUniformBuffer<BackdropTileParameters>();
+                parametersBuffer.Data = new BackdropTileParameters()
+                {
+                    TileScaleX = tileScaleX,
+                    TileScaleY = tileScaleY,
+                    ScrollSpeed = scrollSpeed,
+                    Time = (float)Source.Time.Current
+                };
+
+                shader.BindUniformBlock("m_GridUniforms", parametersBuffer);
+            }
+
+            protected override void Dispose(bool isDisposing)
+            {
+                base.Dispose(isDisposing);
+                parametersBuffer?.Dispose();
+            }
+
+            [StructLayout(LayoutKind.Sequential, Pack = 1)]
+            private record struct BackdropTileParameters
+            {
+                public UniformFloat TileScaleX; // 4
+                public UniformFloat TileScaleY; // 8
+                public UniformFloat ScrollSpeed; // 12
+                public UniformFloat Time; // 16
+                private readonly UniformPadding12 pad1; // 28
+                private readonly UniformPadding4 pad2; // 32
+            }
+        }
+    }
+}
---
+++ LivinOnSweets/LivinOnSweets.Game.Tests/Visual/TestSceneBackdrop.cs
@@ -0,0 +1,39 @@
+﻿using LivinOnSweets.API.Sprites;
+using LivinOnSweets.API.Stores;
+using NUnit.Framework;
+using osu.Framework.Allocation;
+
+namespace LivinOnSweets.Game.Tests.Visual
+{
+    [TestFixture]
+    public partial class TestSceneBackdrop : LivinOnSweetsTestScene
+    {
+        [BackgroundDependencyLoader]
+        private void load(MainMenuStore mmStore)
+        {
+            Backdrop backdrop = new Backdrop()
+            {
+                Texture = mmStore.Get("MainMenu/Patterns/Wireframe.png")
+            };
+            Add(backdrop);
+
+            AddStep("start", backdrop.Start);
+            AddStep("freeze", backdrop.Freeze);
+
+            AddStep("green", () =>
+            {
+                backdrop.Texture = mmStore.Get("MainMenu/Patterns/Green.png");
+            });
+
+            AddStep("blue", () =>
+            {
+                backdrop.Texture = mmStore.Get("MainMenu/Patterns/Blue.png");
+            });
+
+            AddStep("purple", () =>
+            {
+                backdrop.Texture = mmStore.Get("MainMenu/Patterns/Purple.png");
+            });
+        }
+    }
+}
