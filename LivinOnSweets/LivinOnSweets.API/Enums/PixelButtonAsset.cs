bf5709b49bff0b2f27203dadf348c1f29b47954b
sanco <sansunder718jqda@gmail.com>
2025-02-24 01:10:12 +0100

    Add PixelButton with locale support.
    I still need the new toggle sprite though...

---
+++ LivinOnSweets/LivinOnSweets.API/Enums/PixelButtonAsset.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+// ReSharper disable InconsistentNaming
+
+namespace LivinOnSweets.API.Enums
+{
+    // Lists the available options for the pixel button sprites
+    // The descriptions provide the image to retrieve, the path will be built in PixelButton
+    public enum PixelButtonAsset
+    {
+        [Description("Confirm")]
+        CONFIRM,
+
+        [Description("Back")]
+        BACK,
+
+        [Description("Enter")]
+        ENTER,
+
+        [Description("Start")]
+        START,
+
+        [Description("YConfirm")]
+        YELLOW_CONFIRM,
+    }
+}
