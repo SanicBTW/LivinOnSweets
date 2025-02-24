e32c8b37e246b811b0e8d52daa81168345e367f4
sanco <sansunder718jqda@gmail.com>
2025-02-24 02:14:15 +0100

    Change ScreenStackExtensions to CompositeDrawableExtensions.

--- LivinOnSweets/LivinOnSweets.API/Extensions/CompositeDrawableExtensions.cs
+++ LivinOnSweets/LivinOnSweets.API/Extensions/CompositeDrawableExtensions.cs
@@ -1,16 +1,16 @@
 ﻿using System.Reflection;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Screens;
 
 namespace LivinOnSweets.API.Extensions
 {
-    public static class ScreenStackExtensions
+    public static class CompositeDrawableExtensions
     {
         private static MethodInfo removeInternal =
             typeof(CompositeDrawable).GetMethod("RemoveInternal", BindingFlags.Instance | BindingFlags.NonPublic);
 
-        public static void Remove(this ScreenStack screenStack, Drawable drawable, bool disposeImmediately) =>
+        // Changed from ScreenStack to CompositeDrawable, since ScreenStack inherits from it
+        public static void Remove(this CompositeDrawable screenStack, Drawable drawable, bool disposeImmediately) =>
             removeInternal?.Invoke(screenStack, [ drawable, disposeImmediately ]);
     }
 }
