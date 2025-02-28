4b047be92c6aa9a5f9606fea45dadcad59d1d33c
sanco <sansunder718jqda@gmail.com>
2025-03-01 00:55:20 +0100

    Added CakeClearance sprite to represent the Clear and Full Combo milestones.
    what

---
+++ LivinOnSweets/LivinOnSweets.API/Enums/SongClearanceStat.cs
@@ -0,0 +1,11 @@
+﻿// ReSharper disable InconsistentNaming
+namespace LivinOnSweets.API.Enums
+{
+    // Represents the possible CakeClearance Sprite stat to represent (Clear, FC)
+    public enum SongClearanceStat
+    {
+        // Comments describe the spritesheet frame layout
+        CLEAR = 0, // 0(1) not achieved / 1(2) achieved
+        FULL_COMBO = 2, // 2(3) not achieved / 3(4) achieved
+    }
+}
