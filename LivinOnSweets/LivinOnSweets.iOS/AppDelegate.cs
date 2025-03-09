ca7f0cdc64a627b744ca737e25b5d75a41dbdb95
sanco <sansunder718jqda@gmail.com>
2025-03-09 22:20:35 +0100

    Update libraries, fix iOS.

---
+++ LivinOnSweets/LivinOnSweets.iOS/AppDelegate.cs
@@ -0,0 +1,12 @@
+﻿using LivinOnSweets.Game;
+using osu.Framework.iOS;
+
+namespace LivinOnSweets.iOS
+{
+    /// <inheritdoc />
+    public class AppDelegate : GameApplicationDelegate
+    {
+        /// <inheritdoc />
+        protected override osu.Framework.Game CreateGame() => new LivinOnSweetsGame();
+    }
+}
--- LivinOnSweets/LivinOnSweets.iOS/Application.cs
+++ LivinOnSweets/LivinOnSweets.iOS/Application.cs
@@ -1,13 +1,19 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using LivinOnSweets.Game;
-using osu.Framework.iOS;
+using UIKit;
 
 namespace LivinOnSweets.iOS
 {
+    /// <summary>
+    /// </summary>
     public static class Application
     {
-        public static void Main(string[] args) => GameApplication.Main(new LivinOnSweetsGame());
+        /// <summary>
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, typeof(AppDelegate));
+        }
     }
 }
