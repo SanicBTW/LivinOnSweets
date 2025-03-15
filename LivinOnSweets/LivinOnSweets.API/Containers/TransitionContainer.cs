4d369ad9b6d3eeac906aa3d5732f4d89f4a0a295
sanco <sansunder718jqda@gmail.com>
2025-03-15 13:54:18 +0100

    Add Fade Transition Type with parameters support.

--- LivinOnSweets/LivinOnSweets.API/Containers/TransitionContainer.cs
+++ LivinOnSweets/LivinOnSweets.API/Containers/TransitionContainer.cs
@@ -5,6 +5,7 @@ using LivinOnSweets.API.Sprites;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
 using osu.Framework.Screens;
 
 namespace LivinOnSweets.API.Containers
@@ -40,8 +41,10 @@ namespace LivinOnSweets.API.Containers
             ScreenTransitionType transitionType = ev.TransitionData.TransitionType;
             switch (transitionType)
             {
-                // TODO
-                case ScreenTransitionType.BASIC_FADE:
+                case ScreenTransitionType.FADE fadeData:
+                    makeFade(fadeData);
+                    // Push the next screen after fade in
+                    Scheduler.AddDelayed(() => screenStack.Push(ev.TransitionData.NextScreen), fadeData.FadeInDuration);
                     break;
 
                 case ScreenTransitionType.SPRITE_ANIMATED:
@@ -52,6 +55,25 @@ namespace LivinOnSweets.API.Containers
                     });
                     break;
             }
+        }
+
+        private void makeFade(ScreenTransitionType.FADE fadeData)
+        {
+            Box overlay = new Box()
+            {
+                RelativeSizeAxes = Axes.Both,
+                Colour = fadeData.Color,
+                Alpha = 0,
+            };
+
+            Add(overlay);
+
+            overlay.Delay(100D)
+                .FadeInFromZero(fadeData.FadeInDuration, fadeData.Easing)
+                .Then()
+                .FadeOutFromOne(fadeData.FadeOutDuration, fadeData.Easing)
+                .Then()
+                .OnComplete(_ => Remove(overlay, true));
         }
     }
 }
--- LivinOnSweets/LivinOnSweets.API/Enums/ScreenTransitionType.cs
+++ LivinOnSweets/LivinOnSweets.API/Enums/ScreenTransitionType.cs
@@ -1,9 +1,19 @@
 ﻿// ReSharper disable InconsistentNaming
+
+using osu.Framework.Graphics;
+
 namespace LivinOnSweets.API.Enums
 {
-    public enum ScreenTransitionType
+    public abstract class ScreenTransitionType
     {
-        BASIC_FADE,
-        SPRITE_ANIMATED,
+        public class FADE(double fadeInDuration = 800D, double fadeOutDuration = 800D, Colour4? color = null, Easing easing = Easing.OutQuint) : ScreenTransitionType
+        {
+            public readonly double FadeInDuration = fadeInDuration;
+            public readonly double FadeOutDuration = fadeOutDuration;
+            public readonly Colour4 Color = color ?? Colour4.Black;
+            public readonly Easing Easing = easing;
+        }
+
+        public class SPRITE_ANIMATED : ScreenTransitionType { }
     }
 }
