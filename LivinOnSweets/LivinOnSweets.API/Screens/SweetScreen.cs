cc2526a3104b034e1a531a0eefa948366c0dc738
sanco <sansunder718jqda@gmail.com>
2025-05-04 20:20:53 +0200

    Change SweetScreen private screen stack to protected.

--- LivinOnSweets/LivinOnSweets.API/Screens/SweetScreen.cs
+++ LivinOnSweets/LivinOnSweets.API/Screens/SweetScreen.cs
@@ -5,9 +5,9 @@ namespace LivinOnSweets.API.Screens
 {
     public partial class SweetScreen : Screen
     {
-        private SweetScreenStack screenStack => (SweetScreenStack)Parent;
+        protected SweetScreenStack ScreenStack => (SweetScreenStack)Parent;
 
-        public bool IsSubScreenOpen => screenStack.IsSubScreenOpen;
+        public bool IsSubScreenOpen => ScreenStack.IsSubScreenOpen;
 
         public SweetScreen()
         {
@@ -16,41 +16,40 @@ namespace LivinOnSweets.API.Screens
 
         public override bool OnExiting(ScreenExitEvent e)
         {
-            // what is rider suggesting me wtf is this standard?
-            if (screenStack is not { IsSubScreenOpen: true }) return base.OnExiting(e);
+            if (ScreenStack is not { IsSubScreenOpen: true }) return base.OnExiting(e);
 
-            screenStack.ExitSubScreen();
+            ScreenStack.ExitSubScreen();
             return true;
         }
 
         /// <summary>
         /// Calls <see cref="SweetScreenStack.Exit"/>, if a <see cref="SweetSubScreen"/> is open, the current screen will handle exiting the current sub screen.
         /// </summary>
-        protected void Exit() => screenStack.Exit();
+        protected void Exit() => ScreenStack.Exit();
 
         /// <summary>
         /// Calls <see cref="SweetScreenStack.Push"/>
         /// </summary>
         /// <param name="screen">The new screen to add</param>
-        protected void Push(SweetScreen screen) => screenStack.Push(screen);
+        protected void Push(SweetScreen screen) => ScreenStack.Push(screen);
 
         /// <summary>
         /// Calls <see cref="SweetScreenStack.PushSynchronously"/>
         /// </summary>
         /// <param name="screen">The screen to push synchronously</param>
-        protected void PushSynchronously(SweetScreen screen) => screenStack.PushSynchronously(screen);
+        protected void PushSynchronously(SweetScreen screen) => ScreenStack.PushSynchronously(screen);
 
         /// <summary>
         /// Calls <see cref="SweetScreenStack.PushSubScreen"/>
         /// </summary>
         /// <param name="subScreen">The sub screen to push into the sub stack</param>
-        protected void PushSubScreen(SweetSubScreen subScreen) => screenStack.PushSubScreen(subScreen);
+        protected void PushSubScreen(SweetSubScreen subScreen) => ScreenStack.PushSubScreen(subScreen);
 
         /// <summary>
         /// Calls <see cref="SweetScreenStack.ExitSubScreen"/>
         /// <para/>
         /// Another way to exit the SubScreen, if managed properly you can just use <see cref="Exit"/>.
         /// </summary>
-        protected void ExitSubScreen() => screenStack.ExitSubScreen();
+        protected void ExitSubScreen() => ScreenStack.ExitSubScreen();
     }
 }
