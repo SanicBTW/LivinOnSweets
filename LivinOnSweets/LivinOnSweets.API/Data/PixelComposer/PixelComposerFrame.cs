014f8e7d90f24ff5e50a1089137251fc95385138
SanicBTW <sansunder718jqda@gmail.com>
2025-09-16 02:38:29 +0200

    Add PixelComposer 1.17.5 JSON structure.

---
+++ LivinOnSweets/LivinOnSweets.API/Data/PixelComposer/PixelComposerFrame.cs
@@ -0,0 +1,18 @@
+﻿namespace LivinOnSweets.API.Data.PixelComposer
+{
+    /// <summary>
+    /// The structure of a JSON object inside a Pixel Composer Animation structure
+    ///
+    /// <remarks>Structure from Pixel Composer 1.17.5</remarks>
+    /// </summary>
+    public struct PixelComposerFrame()
+    {
+        public int Blend { get; set; } = 0;
+        public float Rotation { get; set; } = 0;
+        public float[] Scale { get; set; } = [];
+        public float[] Size { get; set; } = [];
+        public float Alpha { get; set; } = 0;
+        public PixelComposerSurface Surface { get; set; } = default;
+        public float[] Position { get; set; } = [];
+    }
+}
---
+++ LivinOnSweets/LivinOnSweets.API/Data/PixelComposer/PixelComposerSurface.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace LivinOnSweets.API.Data.PixelComposer
+{
+    /// <summary>
+    /// The structure of the <c>surface</c> object inside <see cref="PixelComposerFrame"/>.
+    ///
+    /// <remarks>Structure from Pixel Composer 1.17.5</remarks>
+    /// </summary>
+    public struct PixelComposerSurface
+    {
+        public int Format { get; set; }
+        public string Surface { get; set; }
+
+        [JsonProperty("w")]
+        public int Width { get; set; }
+
+        [JsonProperty("h")]
+        public int Height { get; set; }
+    }
+}
